8eb1bf0c18b71c923cc22a62154723338337b087
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-10-17 17:22:52 -0400

    format documents

--- script.js
+++ script.js
@@ -138,151 +138,181 @@ var grade4Words = [
   "suburban",
   "metropolitan",
   "agriculture",
-  "rural", "microscope", "microchip", "astrology", "megaphone",
-  "apathy", "sympathy", "hydrate", "dehydrate", "mega", "megabit",
-  "megastar", "astrologist", "astronaut", "hydrologist", "hydroplane",
-  "hydrometer", "search", "church", "branches", "speeches", "beach", "teach", "crouch", "touch", "watches", "fetches", "catches", "dispatch", "choke", "cheerful", "churn", "chosen"];
+  "rural",
+  "microscope",
+  "microchip",
+  "astrology",
+  "megaphone",
+  "apathy",
+  "sympathy",
+  "hydrate",
+  "dehydrate",
+  "mega",
+  "megabit",
+  "megastar",
+  "astrologist",
+  "astronaut",
+  "hydrologist",
+  "hydroplane",
+  "hydrometer",
+  "search",
+  "church",
+  "branches",
+  "speeches",
+  "beach",
+  "teach",
+  "crouch",
+  "touch",
+  "watches",
+  "fetches",
+  "catches",
+  "dispatch",
+  "choke",
+  "cheerful",
+  "churn",
+  "chosen",
+];
 
 function addWordCount() {
-let counter = document.getElementById('wordCountSelect');
-console.log(counter.value);
-wordCount = counter.value;
-if ((wordCount > 4) && (wordCount < 26)) {
-  counterButton = document.getElementById('wordCount');
-  counterButton.style.display = 'none';
-
-generateRan();
-game();
-} else {
-  alert('plese choose a number between 5 and 25!')}};
-
-  //Function generates a random numnber without repetition from the length
-  //of the word list and passes the random numbers to the wordlist[].
-  function generateRan(){
-      var max = grade4Words.length;
-      var random = [];
-      for(var i = 0;i<max ; i++){
-          var temp = Math.floor(Math.random()*max);
-          console.log(`test output of math random ${temp}`)
-          if(random.indexOf(temp) == -1){
-              random.push(temp);
-          }
-          else
-           i--;
-      }
-    let indexOfWord=(random.slice(0,wordCount));
-    indexOfWord.forEach(function (item, index) {
-  wordList.push(grade4Words[item]);
-});
+  let counter = document.getElementById("wordCountSelect");
+  console.log(counter.value);
+  wordCount = counter.value;
+  if (wordCount > 4 && wordCount < 26) {
+    counterButton = document.getElementById("wordCount");
+    counterButton.style.display = "none";
+    generateRan();
+    game();
+  } else {
+    alert("plese choose a number between 5 and 25!");
+  }
 }
 
+//Function generates a random numnber without repetition from the length
+//of the word list and passes the random numbers to the wordlist[].
+function generateRan() {
+  var max = grade4Words.length;
+  var random = [];
+  for (var i = 0; i < max; i++) {
+    var temp = Math.floor(Math.random() * max);
+    console.log(`test output of math random ${temp}`);
+    if (random.indexOf(temp) == -1) {
+      random.push(temp);
+    } else i--;
+  }
+  let indexOfWord = random.slice(0, wordCount);
+  indexOfWord.forEach(function (item, index) {
+    wordList.push(grade4Words[item]);
+  });
+}
 
 //CREATE THE INPUTS FOR THE TEST BASED ON THE WORDLIST
-function game(){
-  let submit = document.getElementById('submitButtonScore');
-  submit.style.display = 'inline-block';
-wordList.forEach(function(value, index){
-  let inputs = document.createElement("input");
-  let btnSpeak = document.createElement("button");
-  var t = document.createTextNode("Listen");
-btnSpeak.appendChild(t);
-  inputs.setAttribute("id", index);
-  btnSpeak.setAttribute("id", value);
-  btnSpeak.classList.add('talkieBtn');
-  inputs.classList.add('xxx');
-  let inputsSection = document.getElementById("inputListSection")
-  inputs.placeholder=index + 1;
-  inputsSection.appendChild(btnSpeak);
-  inputsSection.appendChild(inputs);
-  btnSpeak.addEventListener('click', ()=> {
+function game() {
+  let submit = document.getElementById("submitButtonScore");
+  submit.style.display = "inline-block";
+  wordList.forEach(function (value, index) {
+    let inputs = document.createElement("input");
+    let btnSpeak = document.createElement("button");
+    var t = document.createTextNode("Listen");
+    btnSpeak.appendChild(t);
+    inputs.setAttribute("id", index);
+    btnSpeak.setAttribute("id", value);
+    btnSpeak.classList.add("talkieBtn");
+    inputs.classList.add("xxx");
+    let inputsSection = document.getElementById("inputListSection");
+    inputs.placeholder = index + 1;
+    inputsSection.appendChild(btnSpeak);
+    inputsSection.appendChild(inputs);
+    btnSpeak.addEventListener("click", () => {
       var toSpeak = new SpeechSynthesisUtterance(btnSpeak.id);
-      var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
-      voices.forEach((voice)=>{
-          if(voice.name === selectedVoiceName){
-              toSpeak.voice = voice;
-          }
+      var selectedVoiceName =
+        voiceList.selectedOptions[0].getAttribute("data-name");
+      voices.forEach((voice) => {
+        if (voice.name === selectedVoiceName) {
+          toSpeak.voice = voice;
+        }
       });
       synth.speak(toSpeak);
+    });
   });
-});
-};
+}
 //SUBMIT FUNCTION: CHECK WORDS AGAINST LIST, SCORE AND RETURN SCORE
 function submit() {
-  let scoreBoard = document.getElementById('scoreBoard');
+  let scoreBoard = document.getElementById("scoreBoard");
   scoreBoard.style.display = "flex";
-  let submitButton = document.getElementById('submitButtonScore');
+  let submitButton = document.getElementById("submitButtonScore");
   submitButton.style.display = "none";
-  for (i = 0; i < wordList.length; i++){
-    let decScore = (100/parseInt(wordList.length));
+  for (i = 0; i < wordList.length; i++) {
+    let decScore = 100 / parseInt(wordList.length);
     let box = document.getElementById(i);
     let outputSubmittedWords = box.value.toLowerCase();
     answers.push(outputSubmittedWords);
 
-    if (answers[i] === wordList[i]){
+    if (answers[i] === wordList[i]) {
       let correctAnswer = document.getElementById(i);
-      correctAnswer.classList.add('green');
-    }
-    else{
+      correctAnswer.classList.add("green");
+    } else {
       let incorrectAnswer = document.getElementById(i);
-      incorrectAnswer.classList.add('red');
+      incorrectAnswer.classList.add("red");
       score = score - decScore;
       let box = document.getElementById(i);
       box.value = wordList[i].toUpperCase();
       console.log(wordList[i]);
     }
   }
-  if (score === 100){
-
-     document.getElementById('scoreContainer').innerHTML = (' ' + Math.round(score));
-     var unicorn = document.getElementById('magic');
-     unicorn.style.display = "flex";
-     var audio = document.getElementById('audio');
-     audio.setAttribute("autoplay", true);
-   } else {
-     document.getElementById('scoreContainer').innerHTML = (' ' + Math.round(score));
-     }
-   }
-//CREATE A RESET BUTTON TO RESET THE PAGE BY RELOADING THE URL
-  const refreshButton = document.querySelector('.refresh-button');
-  const refreshPage = () => {
-    location.reload();
+  if (score === 100) {
+    document.getElementById("scoreContainer").innerHTML =
+      " " + Math.round(score);
+    var unicorn = document.getElementById("magic");
+    unicorn.style.display = "flex";
+    var audio = document.getElementById("audio");
+    audio.setAttribute("autoplay", true);
+  } else {
+    document.getElementById("scoreContainer").innerHTML =
+      " " + Math.round(score);
   }
-  refreshButton.addEventListener('click', refreshPage);
+}
+//CREATE A RESET BUTTON TO RESET THE PAGE BY RELOADING THE URL
+const refreshButton = document.querySelector(".refresh-button");
+const refreshPage = () => {
+  location.reload();
+};
+refreshButton.addEventListener("click", refreshPage);
 
-       var txtInput = document.querySelector('#txtInput');
-       var voiceList = document.querySelector('#voiceList');
-       var btnSpeak = document.querySelector('#btnSpeak');
-       var wordPrompt = document.getElementsByClassName('wordPrompt');
-       var synth = window.speechSynthesis;
-       var voices = [];
+var txtInput = document.querySelector("#txtInput");
+var voiceList = document.querySelector("#voiceList");
+var btnSpeak = document.querySelector("#btnSpeak");
+var wordPrompt = document.getElementsByClassName("wordPrompt");
+var synth = window.speechSynthesis;
+var voices = [];
 
-       PopulateVoices();
-       if(speechSynthesis !== undefined){
-           speechSynthesis.onvoiceschanged = PopulateVoices;
-       }
+PopulateVoices();
+if (speechSynthesis !== undefined) {
+  speechSynthesis.onvoiceschanged = PopulateVoices;
+}
 
-       btnSpeak.addEventListener('click', ()=> {
-           var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
-           var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
-           voices.forEach((voice)=>{
-               if(voice.name === selectedVoiceName){
-                   toSpeak.voice = voice;
-               }
-           });
-           synth.speak(toSpeak);
-       });
+btnSpeak.addEventListener("click", () => {
+  var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
+  var selectedVoiceName =
+    voiceList.selectedOptions[0].getAttribute("data-name");
+  voices.forEach((voice) => {
+    if (voice.name === selectedVoiceName) {
+      toSpeak.voice = voice;
+    }
+  });
+  synth.speak(toSpeak);
+});
 
-       function PopulateVoices(){
-           voices = synth.getVoices();
-           var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
-           voiceList.innerHTML = '';
-           voices.forEach((voice)=>{
-               var listItem = document.createElement('option');
-               listItem.textContent = voice.name;
-               listItem.setAttribute('data-lang', voice.lang);
-               listItem.setAttribute('data-name', voice.name);
-               voiceList.appendChild(listItem);
-           });
+function PopulateVoices() {
+  voices = synth.getVoices();
+  var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex;
+  voiceList.innerHTML = "";
+  voices.forEach((voice) => {
+    var listItem = document.createElement("option");
+    listItem.textContent = voice.name;
+    listItem.setAttribute("data-lang", voice.lang);
+    listItem.setAttribute("data-name", voice.name);
+    voiceList.appendChild(listItem);
+  });
 
-           voiceList.selectedIndex = selectedIndex;
-       }
+  voiceList.selectedIndex = selectedIndex;
+}
